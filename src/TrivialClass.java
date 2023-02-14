637965e8eaba15f6c8ae8851cb447f99708b6c02
Héctor Vallvé <hectorvallveg@gmail.com>
2023-02-14 17:59:13 +0100

    asd

--- src/TrivialClass.java
+++ src/TrivialClass.java
@@ -7,6 +7,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +22,18 @@ import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 public class TrivialClass extends JFrame {
+	private int turno = 0, posicionJugador1 = 0, posicionJugador2 = 0, puntuacionJugador1 = 0, puntuacionJugador2 = 0;
+	private JPanel tablero = new JPanel();
+	private BufferedImage jugador1 = null;
+	private BufferedImage jugador2 = null;
+	private BufferedImage jugadorMeta1 = null;
+	private BufferedImage jugadorMeta2 = null;
+	private BufferedImage pasto = null;
+	private BufferedImage meta = null;
+
+	private JLabel imagenesJugador1[] = new JLabel[8];
+	private JLabel imagenesJugador2[] = new JLabel[8];
+
 	public TrivialClass() {
 
 		super("Trivial");
@@ -31,14 +45,13 @@ public class TrivialClass extends JFrame {
 
 		JPanel panel = new JPanel();
 		Container cp = getContentPane();
-		JPanel tablero = new JPanel();
 
 		cp.setLayout(new GridBagLayout());
 		cp.setBackground(Color.decode("#E2F99D"));
 
 		tablero.setLayout(new GridLayout(2, 1));
 		panel.setLayout(new GridBagLayout());
-		
+
 		GridBagConstraints posicionBoton = new GridBagConstraints();
 		posicionBoton.gridx = 0;
 		posicionBoton.gridy = 0;
@@ -74,10 +87,10 @@ public class TrivialClass extends JFrame {
 		posicionPanel.gridy = 0;
 		posicionPanel.insets = new Insets(0, 0, 320, 150);
 
-		int turno = 0, puntuacionJugador1 = 0, puntuacionJugador2 = 0;
 		JLabel textoJugador = new JLabel(" Jugador: asdasd ");
 		JLabel textoTurno = new JLabel("Turno: " + turno);
-		JLabel textoPuntuacion = new JLabel("Puntuacion: Jugador1 " + puntuacionJugador1 + " Jugador2 " + puntuacionJugador2);
+		JLabel textoPuntuacion = new JLabel(
+				"Puntuacion: Jugador1 " + puntuacionJugador1 + " Jugador2 " + puntuacionJugador2);
 
 		textoJugador.setFont(new Font("Sans-Serif", Font.BOLD, 18));
 		textoTurno.setFont(new Font("Sans-Serif", Font.BOLD, 18));
@@ -90,16 +103,14 @@ public class TrivialClass extends JFrame {
 		panelTextoJugador.setLayout(new GridBagLayout());
 		panelTextoJugador.setBorder(borde);
 
-		BufferedImage jugador1 = null;
-		BufferedImage jugador2 = null;
-		BufferedImage pasto = null;
-		BufferedImage meta = null;
-
 		try {
 			jugador1 = ImageIO.read(new File("caballo.PNG"));
 			jugador2 = ImageIO.read(new File("caballo2.PNG"));
+			jugadorMeta1 = ImageIO.read(new File("caballoMeta.PNG"));
+			jugadorMeta2 = ImageIO.read(new File("caballoMeta2.PNG"));
 			pasto = ImageIO.read(new File("pasto.jpg"));
 			meta = ImageIO.read(new File("meta.jpg"));
+			
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -107,7 +118,7 @@ public class TrivialClass extends JFrame {
 		}
 
 		JPanel panelTextoTurno = new JPanel();
-		
+
 		panelTextoTurno.setBackground(Color.decode("#CDFA4A"));
 		panelTextoTurno.setBorder(borde);
 		panelTextoTurno.add(textoTurno);
@@ -120,14 +131,10 @@ public class TrivialClass extends JFrame {
 
 		Border bordeTablero = BorderFactory.createLineBorder(Color.black);
 
-		JLabel imagenesJugador1[] = new JLabel[8];
-		JLabel imagenesJugador2[] = new JLabel[8];
-		
-
 		imagenesJugador1[0] = new JLabel(new ImageIcon(jugador1));
 		imagenesJugador2[0] = new JLabel(new ImageIcon(jugador2));
 
-		for (int i = 1; i < (imagenesJugador1.length - 1) ; i++) {
+		for (int i = 1; i < (imagenesJugador1.length - 1); i++) {
 
 			imagenesJugador1[i] = new JLabel(new ImageIcon(pasto));
 			imagenesJugador2[i] = new JLabel(new ImageIcon(pasto));
@@ -135,12 +142,12 @@ public class TrivialClass extends JFrame {
 		}
 		imagenesJugador1[7] = new JLabel(new ImageIcon(meta));
 		imagenesJugador2[7] = new JLabel(new ImageIcon(meta));
-		
-		
+
 		Button botonPregunta = new Button("Preguntas");
 		botonPregunta.setPreferredSize(new Dimension(10, 10));
 		botonPregunta.setBackground(Color.decode("#CDFA4A"));
 		botonPregunta.setFont(new Font("Sans-Serif", Font.BOLD, 18));
+		botonPregunta.addActionListener(new EventAumentarPosicioJugador());
 
 		for (int i = 0; i < imagenesJugador1.length; i++) {
 			imagenesJugador1[i].setPreferredSize(new Dimension(100, 40));
@@ -164,6 +171,35 @@ public class TrivialClass extends JFrame {
 		cp.add(panelTextoTurno, posicionTurno);
 		cp.add(tablero, posicionTablero);
 		cp.add(botonPregunta, posicionBoton);
+
+	}
+
+	// Recoger
+	class EventAumentarPosicioJugador implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (turno % 2 == 0) {
+				imagenesJugador1[posicionJugador1].setIcon(new ImageIcon(pasto));
+				posicionJugador1++;
+				imagenesJugador1[posicionJugador1].setIcon(new ImageIcon(jugador1));
+				System.out.println(posicionJugador1);
+			} else {
+				imagenesJugador2[posicionJugador2].setIcon(new ImageIcon(pasto));
+				posicionJugador2++;
+				imagenesJugador2[posicionJugador2].setIcon(new ImageIcon(jugador2));
+				System.out.println(posicionJugador2);
+			}
+			turno++;
+			if (posicionJugador1 == 7) {
+
+				imagenesJugador1[posicionJugador1].setIcon(new ImageIcon(jugadorMeta1));
+
+			}else if(posicionJugador2 == 7) {
+				imagenesJugador2[posicionJugador2].setIcon(new ImageIcon(jugadorMeta2));
+
+			}
+
+		}
 
 	}
 
